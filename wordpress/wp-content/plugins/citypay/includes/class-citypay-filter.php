530234dfc021187ab8cc463745fd21bad8492e96
Roger Bisson <roger.bisson@citypay.com>
2015-01-09 14:46:17 +0000

    Moved functions concerned with content processing / post-processing and filtering to class-citypay-filter.php to improve clarity of main plugin source file.

---
+++ wordpress/wp-content/plugins/citypay/includes/class-citypay-filter.php
@@ -0,0 +1,193 @@
+<?php
+
+class CityPay_Filter {
+    
+    function cp_paylink_get_tag($s, &$i, &$i_max)
+    {
+        $j = $i++;
+
+        $c = $s[$i];
+        if ($c != "/") {
+            $tag_type = CP_PAYLINK_OPENING_TAG;
+        } else {
+            $tag_type = CP_PAYLINK_CLOSING_TAG;
+            $i++;
+        }
+
+        $tag = '';
+        while ($i < $i_max) {
+            $c = $s[$i++];
+            if ($c == ">") {
+                break;
+            } else if ($c == " " || $c == "/r" || $c == "/n" || $c == "/t") { 
+                break;
+            } else {
+                $tag .= $c;
+                $i++;
+            }
+        }
+
+        $attrs = array();
+        if ($c == " " || $c == "/r" || $c == "/n" || $c == "/t")
+        {
+            while ($i < $i_max)
+            {
+                // purge whitespace
+                while (++$i < $i_max) {
+                    $c = $s[$i];
+                    if ($c != " " && $c != "/r" && $c != "/n" && $c != "/t") {
+                        break;
+                    }
+                }
+
+                if ($c == "/" && $tag_type == CP_PAYLINK_OPENING_TAG) {
+                    $tag_type = CP_PAYLINK_SELF_CLOSING_TAG;
+                }
+
+                $attr_name = '';
+                $attr_value = null;
+                while ($i < $i_max) {
+                    $c = $s[$i];
+                    if ($c == "=" || $c == " " || $c == "/r" || $c == "/n" || $c == "/t") {
+                        break;
+                    } else {
+                        $attr_name .= $c;
+                        $i++;
+                    }
+                }
+
+                if ($c == "=") { 
+                    $i++;
+                    $attr_value = '';
+                    while ($i < $i_max) {
+                        $c = $s[$i];
+                        if ($c == " " || $c == "/r" || $c == "/n" || $c == "/t") {
+                            break;
+                        } else {
+                            $attr_value .= $c;
+                            $i++;
+                        }
+                    }
+                }
+
+                $attrs[] = new attr($attr_name, $attr_value);
+            }
+        }
+
+        if ($c == ">")
+        {
+            switch ($tag_type)
+            {
+            case CP_PAYLINK_OPENING_TAG:
+                //$stack[] = new tag($tag, $)
+                break;
+
+            case CP_PAYLINK_SELF_CLOSING_TAG:
+            case CP_PAYLINK_CLOSING_TAG:
+
+                break;
+            }
+        }
+
+        $tag_lc = strtolower($tag);
+        $tag_obj = tag($tag_lc, $attrs, $j, $i, $tag_type);
+    }
+
+    function cp_paylink_trim_outer_p_and_br_tags($s) {
+
+        $stack = array();
+        $content = array();
+
+        $i = 0;
+        $i_max = strlen($s);
+        while ($i < $i_max) {
+            // purge whitespace
+            while ($i < $i_max) {
+                $c = $s[$i++];
+                if ($c != " " && $c != "\n" && $c != "\r" && $c != "\t") { break; }
+            }
+
+            if ($i >= $i_max) { break; }
+
+            if ($c == "<") {
+                $stack[] = &$tag_obj;
+                $content[] = &$tag_obj;
+            } else {
+                /*while ($i < $i_max) {
+                    if 
+                }*/
+            }
+        }
+    }
+
+
+    function cp_paylink_trim_p_and_br_tags($s) {
+        $i = 0;
+        $i_max = strlen($s);
+        while ($i < $i_max) {
+            $c = $s[$i];
+            if ($c == "<") {
+                $k = $i++;
+                $tag = '';
+                while ($i < $i_max) {
+                    $c = $s[$i++];
+                    if ($c != ">") {
+                        $tag .= $c;
+                    } else {
+                        break;
+                    }
+                }
+                $tag_lc = strtolower($tag);
+                if ($tag_lc != "br" && $tag_lc != "br/" && $tag_lc != "br /") {
+                    $i = $k;
+                    break;
+                }
+            } else if ($c == " " || $c == "\n" || $c == "\r" || $c == "\t") {
+                // do nothing
+                $i++;
+            } else {
+                break;
+            }
+        }
+
+        $j = $i_max - 1;
+        while ($j > $i) {
+            $c = $s[$j];
+            if ($c == ">") {
+                $k = $j + 1;
+                $tag = "";
+                while ($j > $i) {
+                    $c = $s[$j--];
+                    if ($c != "<") {
+                        $tag .= $c;
+                    } else {
+                        break;
+                    }
+                }
+                $tag_lcr = strrev(strtolower($tag));
+                if ( $tag_lcr != "br" && $tag_lcr != "br/" && $tag_lcr != "br /") {
+                    $j = $k;
+                    break;
+                }
+            } else if ($c == " " || $c == "\n" || $c == "\r" || $c == "\t") {
+                // do nothing
+                $j--;
+            } else {
+                break;
+            }
+        }
+
+        /*echo '<pre>';
+        var_dump(bin2hex($s));
+        var_dump($i_max);
+        var_dump($i);
+        var_dump($j);
+        var_dump(bin2hex(substr($s, $i, ($j - $i))));
+        var_dump(bin2hex(substr($s, 0, $j + 1)));
+        echo '</pre>';*/
+
+        return substr($s, $i, ($j - $i));
+    }
+    
+}
+
